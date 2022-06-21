6aff33ba207c3cc771d8efc8e4480cbfd7cd08c5
faizmaruf <faiz.maruf8888@gmail.com>
2022-06-21 21:56:00 +0700

    first commit

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
-const judul = document.getElementsByTagName('h1')[0];
-judul.innerHTML = 'Faiz ganteng';
+// const judul = document.getElementsByTagName('h1')[0];
+// judul.innerHTML = 'Faiz ganteng';
+
+// const p = document.querySelector('div.kelas1 p');
+const p = document.getElementsByClassName("kelas1");
+// p.innerHTML = "ardi ganteng";
+const k = document.getElementsByClassName(".kelas2");
