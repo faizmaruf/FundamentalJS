d1227f480ef1398201348b5a0c7497498ac2f79e
faizmaruf <faiz.maruf8888@gmail.com>
2022-06-22 20:11:39 +0700

    update

--- script.js
+++ script.js
@@ -1,7 +1,2 @@
-// const judul = document.getElementsByTagName('h1')[0];
-// judul.innerHTML = 'Faiz ganteng';
-
-// const p = document.querySelector('div.kelas1 p');
-const p = document.getElementsByClassName("kelas1");
-// p.innerHTML = "ardi ganteng";
-const k = document.getElementsByClassName(".kelas2");
+const modal = document.getElementById("modal");
+const p = document.getElementById("modal");
