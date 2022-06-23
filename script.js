5c6dc9cd9b33a6a06c25c823bbd5314a2967570f
faizmaruf <faiz.maruf8888@gmail.com>
2022-06-23 21:47:24 +0700

    update

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 const modal = document.getElementById("modal");
+const container = document.getElementById("container");
 // modal.className += "hiden";
-console.log(modal);
+// console.log(modal);
+modal.remove();
+
+
+function(){
+container.insertBefore(modal, container.children[0]);
+}
