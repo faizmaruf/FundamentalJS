fb0c337b3fe2e5283e5e17a4ca8945ef0675d16b
faizmaruf <faiz.maruf8888@gmail.com>
2022-06-24 22:14:29 +0700

    update

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-const modal = document.getElementById("modal");
-const container = document.getElementById("container");
-// modal.className += "hiden";
-// console.log(modal);
-modal.remove();
+// const modal = document.getElementById("modal");
+// const container = document.getElementById("container");
+// // modal.className += "hiden";
+// // console.log(modal);
+// modal.remove();
 
 // container.insertBefore(modal, container.children[0]);
 
 function myFunc() {
-  //
+  const btn = document.getElementById("btn");
+  if (btn.style.display === "none") {
+    btn.style.display = "block";
+  } else {
+    btn.style.display = "none";
+  }
 }
+const btn1 = document.getElementById("btn");
+btn1.addEventListener("click", myFunc());
