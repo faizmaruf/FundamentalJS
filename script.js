5aedf368be6614013866b980baae78d9d602be1a
faizmaruf <faiz.maruf8888@gmail.com>
2022-06-26 08:04:22 +0700

    update

--- script.js
+++ script.js
@@ -6,13 +6,13 @@
 
 // container.insertBefore(modal, container.children[0]);
 
-function myFunc() {
-  const btn = document.getElementById("btn");
-  if (btn.style.display === "none") {
-    btn.style.display = "block";
-  } else {
-    btn.style.display = "none";
-  }
-}
-const btn1 = document.getElementById("btn");
-btn1.addEventListener("click", myFunc());
+// function myFunc() {
+//   const btn = document.getElementById("btn");
+//   if (btn.style.display === "none") {
+//     btn.style.display = "block";
+//   } else {
+//     btn.style.display = "none";
+//   }
+// }
+// const btn1 = document.getElementById("btn");
+// btn1.addEventListener("click", myFunc());
