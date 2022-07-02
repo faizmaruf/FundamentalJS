37c0143d5e8419e54b79b0d5a0fde0f0e1281eba
faizmaruf <faiz.maruf8888@gmail.com>
2022-07-02 21:54:28 +0700

    update

--- script.js
+++ script.js
@@ -19,8 +19,27 @@
 // console.log(document);
 // console.log("faiz");
 // console.log("ganteng");
-let i = 0;
-while (i < 10) {
-  console.log("faiz ganteng");
-  i++;
+// let i = 0;
+// while (i < 10) {
+//   console.log("faiz ganteng" + " " + i);
+//   i++;
+// }
+
+let arr = [9, 2, 3, 5, 1, 77, 5, 66, 90];
+
+console.log(arr);
+function bubbleSort(arr) {
+  let temp;
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i; j < arr.length; j++) {
+      if (arr[i] > arr[j]) {
+        temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+  return arr;
 }
+
+console.log(bubbleSort(arr));
