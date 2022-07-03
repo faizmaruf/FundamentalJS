575ec0cec4b725de906b7e7e9258e833de7504da
faizmaruf <faiz.maruf8888@gmail.com>
2022-07-03 23:01:55 +0700

    update

--- script.js
+++ script.js
@@ -25,21 +25,21 @@
 //   i++;
 // }
 
-let arr = [9, 2, 3, 5, 1, 77, 5, 66, 90];
+// let arr = [9, 2, 3, 5, 1, 77, 5, 66, 90];
 
-console.log(arr);
-function bubbleSort(arr) {
-  let temp;
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = i; j < arr.length; j++) {
-      if (arr[i] > arr[j]) {
-        temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-      }
-    }
-  }
-  return arr;
-}
+// console.log(arr);
+// function bubbleSort(arr) {
+//   let temp;
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let j = i; j < arr.length; j++) {
+//       if (arr[i] > arr[j]) {
+//         temp = arr[i];
+//         arr[i] = arr[j];
+//         arr[j] = temp;
+//       }
+//     }
+//   }
+//   return arr;
+// }
 
-console.log(bubbleSort(arr));
+// console.log(bubbleSort(arr));
