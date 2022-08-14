14ffbff99b1a884c7ea4ca6574b7e930e851cbf2
faizmaruf <faiz.maruf8888@gmail.com>
2022-08-15 06:40:42 +0700

    update

--- script.js
+++ script.js
@@ -1,45 +1,18 @@
-// const modal = document.getElementById("modal");
-// const container = document.getElementById("container");
-// // modal.className += "hiden";
-// // console.log(modal);
-// modal.remove();
+// Two Sum (mencari indeks yang memiliki nilai jika ditambahkan sama dengan target)
+let arr = [2, 7, 11, 15];
+let target = 9;
 
-// container.insertBefore(modal, container.children[0]);
-
-// function myFunc() {
-//   const btn = document.getElementById("btn");
-//   if (btn.style.display === "none") {
-//     btn.style.display = "block";
-//   } else {
-//     btn.style.display = "none";
-//   }
-// }
-// const btn1 = document.getElementById("btn");
-// // btn1.addEventListener("click", myFunc());
-// console.log(document);
-// console.log("faiz");
-// console.log("ganteng");
-// let i = 0;
-// while (i < 10) {
-//   console.log("faiz ganteng" + " " + i);
-//   i++;
-// }
-
-let arr = [9, 2, 3, 5, 1, 77, 5, 66, 90];
-
-console.log(arr);
-function bubbleSort(arr) {
-  let temp;
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = i; j < arr.length; j++) {
-      if (arr[i] > arr[j]) {
-        temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
+let twoSum = function (arr, target) {
+  let arr1 = [];
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = i; j < arr.length - 1; j++) {
+      if (arr[i] + arr[j + 1] == target) {
+        arr1.push(i);
+        arr1.push(j + 1);
       }
     }
   }
-  return arr;
-}
-
-console.log(bubbleSort(arr));
+  return arr1;
+};
+let k = twoSum(arr, target);
+console.table(k);
